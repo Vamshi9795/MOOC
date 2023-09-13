78bef4f9f8df5d20b2125edd45d045d88a9e51a2
Vamshi9795 <chjyothendar@gmail.com>
2023-09-13 11:20:26 -0500

    added question

--- quadratic equation.py
+++ quadratic equation.py
@@ -1,3 +1,7 @@
+# Please write a program for solving a quadratic equation of the form ax²+bx+c. The program asks for values a, b and c. It should then use the quadratic formula to solve the equation. The quadratic formula expressed with the Python sqrt function is as follows:
+
+# x = (-b ± sqrt(b²-4ac))/(2a).
+
 from math import sqrt
 a = float(input(" Enter the value of a : "))
 b = float(input(" Enter the value of b : "))
