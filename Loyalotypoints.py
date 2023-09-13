e35040e8cbb33c618160bc98b8d198412e4ee7f3
Vamshi9795 <chjyothendar@gmail.com>
2023-09-13 10:23:38 -0500

    added condition statemet

--- Loyalotypoints.py
+++ Loyalotypoints.py
@@ -16,12 +16,23 @@
 # Your bonus is 15 %
 # You now have 120.175 points
 # Fix the program
+# points = int(input("How many points are on your card? "))
+# if points < 100:
+#     print("Your bonus is 10 %")
+#     points *= 1.1
+# else:
+#     print("Your bonus is 15 %")
+#     points *= 1.15
+
+# print("You now have", points, "points")
 points = int(input("How many points are on your card? "))
-if points < 100:
+condition = points < 100
+condition2 = points >= 100
+if condition:
     points *= 1.1
     print("Your bonus is 10 %")
 
-if points >= 100:
+if condition2:
     points *= 1.15
     print("Your bonus is 15 %")
 
