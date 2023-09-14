bdfd7bf75f0a6900e01f4408955d5bdedd7e721a
Vamshi9795 <chjyothendar@gmail.com>
2023-09-14 13:23:37 -0500

    added questions

---
+++ number of characters.py
@@ -0,0 +1,16 @@
+<!-- Please write a program which asks the user for a word and then prints out the number of characters, if there was more than one typed in.
+
+Examples of expected behaviour:
+
+Sample output
+Please type in a word: hey
+There are 3 letters in the word hey
+Thank you!
+Sample output
+Please type in a word: banana
+There are 6 letters in the word banana
+Thank you!
+Sample output
+Please type in a word: b
+Thank you! -->
+
