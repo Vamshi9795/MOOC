3e930c0ad3a0ae68f8da2223539a220fee843900
Vamshi9795 <chjyothendar@gmail.com>
2023-09-14 22:45:22 -0500

    added question

---
+++ greaterthanornot.py
@@ -0,0 +1,16 @@
+<!-- Please write a program which asks for two integer numbers. The program should then print out whichever is greater. If the numbers are equal, the program should print a different message.
+
+Some examples of expected behaviour:
+
+Sample output
+Please type in the first number: 5
+Please type in another number: 3
+The greater number was: 5
+Sample output
+Please type in the first number:: 5
+Please type in another number: 8
+The greater number was: 8
+Sample output
+Please type in the first number: 5
+Please type in another number: 5
+The numbers are equal! -->
