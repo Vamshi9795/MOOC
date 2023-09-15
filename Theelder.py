ae13934680f34767d797d4a2028a0d94bf5f99f9
Vamshi9795 <chjyothendar@gmail.com>
2023-09-15 16:16:53 -0500

    added quesion

---
+++ Theelder.py
@@ -0,0 +1,20 @@
+Please write a program which asks for the names and ages of two persons. The program should then print out the name of the elder.
+
+Some examples of expected behaviour:
+
+Sample output
+Person 1:
+Name: Alan
+Age: 26
+Person 2:
+Name: Ada
+Age: 27
+The elder is Ada
+Sample output
+Person 1:
+Name: Bill
+Age: 1
+Person 2:
+Name: Jean
+Age: 1
+Bill and Jean are the same age
