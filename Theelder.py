6f9759172f05a1ce96ea6a28cbab4537ddfd6c05
Vamshi9795 <chjyothendar@gmail.com>
2023-09-15 16:20:05 -0500

    statemts to enter the name and age

--- Theelder.py
+++ Theelder.py
@@ -1,20 +1,27 @@
-Please write a program which asks for the names and ages of two persons. The program should then print out the name of the elder.
+# Please write a program which asks for the names and ages of two persons. The program should then print out the name of the elder.
 
-Some examples of expected behaviour:
+# Some examples of expected behaviour:
 
-Sample output
-Person 1:
-Name: Alan
-Age: 26
-Person 2:
-Name: Ada
-Age: 27
-The elder is Ada
-Sample output
-Person 1:
-Name: Bill
-Age: 1
-Person 2:
-Name: Jean
-Age: 1
-Bill and Jean are the same age
+# Sample output
+# Person 1:
+# Name: Alan
+# Age: 26
+# Person 2:
+# Name: Ada
+# Age: 27
+# The elder is Ada
+# Sample output
+# Person 1:
+# Name: Bill
+# Age: 1
+# Person 2:
+# Name: Jean
+# Age: 1
+# Bill and Jean are the same age
+
+person1_age = int(input(" Enter the age of person1"))
+person2_name = (input(" Enter the age of person1"))
+person2_age = int(input(" Enter the age of person2"))
+person2_name = (input(" Enter the age of person2"))
+#  statemet to input the name and age of the persons
+
