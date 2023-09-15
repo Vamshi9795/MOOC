b86bac0179ecb4f7bce9b99d44e54ef27abebd89
Vamshi9795 <chjyothendar@gmail.com>
2023-09-15 16:09:25 -0500

    writeed input statement

--- greaterthanornot.py
+++ greaterthanornot.py
@@ -1,16 +1,19 @@
-<!-- Please write a program which asks for two integer numbers. The program should then print out whichever is greater. If the numbers are equal, the program should print a different message.
+# <!-- Please write a program which asks for two integer numbers. The program should then print out whichever is greater. If the numbers are equal, the program should print a different message.
 
-Some examples of expected behaviour:
+# Some examples of expected behaviour:
 
-Sample output
-Please type in the first number: 5
-Please type in another number: 3
-The greater number was: 5
-Sample output
-Please type in the first number:: 5
-Please type in another number: 8
-The greater number was: 8
-Sample output
-Please type in the first number: 5
-Please type in another number: 5
-The numbers are equal! -->
+# Sample output
+# Please type in the first number: 5
+# Please type in another number: 3
+# The greater number was: 5
+# Sample output
+# Please type in the first number:: 5
+# Please type in another number: 8
+# The greater number was: 8
+# Sample output
+# Please type in the first number: 5
+# Please type in another number: 5
+# The numbers are equal! -->
+
+a = int(input(" enter the first number :"))
+b = int(input(" Enter the second number :"))
