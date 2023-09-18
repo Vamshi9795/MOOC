344e1ac71f1202ee7581a2f6b52afa0e9f4ba684
Vamshi9795 <chjyothendar@gmail.com>
2023-09-17 21:37:43 -0500

    added question

---
+++ gradesandpoints.py
@@ -0,0 +1,22 @@
+# The table below outlines the grade boundaries on a certain university course. Please write a program which asks for the amount of points received and then prints out the grade attained according to the table.
+
+# points	grade
+# < 0	impossible!
+# 0-49	fail
+# 50-59	1
+# 60-69	2
+# 70-79	3
+# 80-89	4
+# 90-100	5
+# > 100	impossible!
+# Some examples:
+
+# Sample output
+# How many points [0-100]: 37
+# Grade: fail
+# Sample output
+# How many points [0-100]: 76
+# Grade: 3
+# Sample output
+# How many points [0-100]: -3
+# Grade: impossible!
