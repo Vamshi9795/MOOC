7b09ad1dad6f900d67f0ed4949c54019e447799b
Vamshi9795 <chjyothendar@gmail.com>
2023-09-23 13:09:19 -0500

    added question

---
+++ story.py
@@ -0,0 +1,35 @@
+# Please write a program which keeps asking the user for words. If the user types in end, the program should print out the story the words formed, and finish.
+
+# Sample output
+# Please type in a word: Once
+# Please type in a word: upon
+# Please type in a word: a
+# Please type in a word: time
+# Please type in a word: there
+# Please type in a word: was
+# Please type in a word: a
+# Please type in a word: girl
+# Please type in a word: end
+# Once upon a time there was a girl
+# Part 2
+
+# Change the program so that the loop ends also if the user types in the same word twice.
+
+# Sample output
+# Please type in a word: It
+# Please type in a word: was
+# Please type in a word: a
+# Please type in a word: dark
+# Please type in a word: and
+# Please type in a word: stormy
+# Please type in a word: night
+# Please type in a word: night
+# It was a dark and stormy night
+
+a = input(" Please type in a word ").lower()
+while True:
+    if a == "end":
+        break
+    else:
+        a += a
+        print(f" The story is ")
