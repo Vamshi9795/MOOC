8f368ff58392f1807c129821e3cc1d5522230a03
Vamshi9795 <chjyothendar@gmail.com>
2023-09-24 19:57:26 -0500

    added wtry and exceot

--- whilenumbers.py
+++ whilenumbers.py
@@ -1,9 +1,19 @@
-Please write a program which asks the user for a number. The program then prints out all integer numbers greater than zero but smaller than the input.
+# Please write a program which asks the user for a number. The program then prints out all integer numbers greater than zero but smaller than the input.
 
-Sample output
-Upper limit: 5
-1
-2
-3
-4
-Please don't use the value True as the condition of your while loop in this exercise!
+# Sample output
+# Upper limit: 5
+# 1
+# 2
+# 3
+# 4
+# Please don't use the value True as the condition of your while loop in this exercise!
+
+while True:
+    try:
+        a = int(input(" Enter the number"))
+        print(a)
+        
+        
+        
+    except ValueError:
+        print(" Finished execution")
