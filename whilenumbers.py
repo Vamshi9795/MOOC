5a9f4e69767f31024308ad731f045e8d916a99cc
Vamshi9795 <chjyothendar@gmail.com>
2023-09-24 19:55:31 -0500

    added question

---
+++ whilenumbers.py
@@ -0,0 +1,9 @@
+Please write a program which asks the user for a number. The program then prints out all integer numbers greater than zero but smaller than the input.
+
+Sample output
+Upper limit: 5
+1
+2
+3
+4
+Please don't use the value True as the condition of your while loop in this exercise!
