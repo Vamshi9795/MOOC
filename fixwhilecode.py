2883a3b1249c77acb7d653ade78fa22b56d16d0c
Vamshi9795 <chjyothendar@gmail.com>
2023-09-24 19:44:25 -0500

    fixed code

--- fixwhilecode.py
+++ fixwhilecode.py
@@ -1,19 +1,26 @@
-The program below has some syntactic issues:
+# The program below has some syntactic issues:
+
+# print("Are you ready?")
+# number = int(input("Please type in a number: "))
+# while number = 0:
+# print(number)
+# print("Now!")
+# Please fix it so that it prints out the following:
+
+# Sample output
+# Are you ready?
+# Please type in a number: 5
+# 5
+# 4
+# 3
+# 2
+# 1
+# Now!
+# This exercise is similar to the countdown exercise in the last section, but please don't use a while True loop this time round!
 
 print("Are you ready?")
 number = int(input("Please type in a number: "))
-while number = 0:
-print(number)
-print("Now!")
-Please fix it so that it prints out the following:
-
-Sample output
-Are you ready?
-Please type in a number: 5
-5
-4
-3
-2
-1
-Now!
-This exercise is similar to the countdown exercise in the last section, but please don't use a while True loop this time round!
+while number > 0:
+    number-=1
+    print(number)
+    print("Now!")
