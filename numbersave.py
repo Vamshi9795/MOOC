9b4f46f16e8c5d34797dcc4c27ea18c48216843f
Vamshi9795 <chjyothendar@gmail.com>
2023-09-24 19:30:33 -0500

    added queestion

---
+++ numbersave.py
@@ -0,0 +1,14 @@
+# # Print numbers until the variable a equals 5
+# a = 1
+# while True:
+#     a+=1
+#     print(a)
+#     if a == 5:
+#         break
+
+num = int(input(" Enther the number"))
+while num < 100:
+    num +=1
+    print(num)
+    if num == 50:
+        break
