455f21bfa740bdae680379000f8fdd8a8fe9ad64
Vamshi9795 <chjyothendar@gmail.com>
2023-09-26 13:09:30 -0500

    added line to take input

---
+++ lengthofstring.py
@@ -0,0 +1,19 @@
+# Please write a program which asks the user for two strings and then prints out whichever is the longer of the two - that is, whichever has the more characters. If the strings are of equal length, the program should print out "The strings are equally long".
+
+# Some examples of expected behaviour:
+
+# Sample output
+# Please type in string 1: hey
+# Please type in string 2: hiya
+# hiya is longer
+# Sample output
+# Please type in string 1: howdy doody
+# Please type in string 2: hola
+# howdy doody is longer
+# Sample output
+# Please type in string 1: hey
+# Please type in string 2: bye
+# The strings are equally long
+
+str1 = input(" Enter the first word: ") # used to take the input
+str2 = input(" Enter the second word: ") # used to take the input
