2d04e1c963f302471ad8138a0dc0aaf51b7ad47e
Vamshi9795 <chjyothendar@gmail.com>
2023-09-26 13:24:50 -0500

    added code

--- endtobegg.py
+++ endtobegg.py
@@ -1,10 +1,16 @@
-Please write a program which asks the user for a string. The program then prints out the input string in reversed order, from end to beginning. Each character should be on a separate line.
+# Please write a program which asks the user for a string. The program then prints out the input string in reversed order, from end to beginning. Each character should be on a separate line.
 
-An example of expected behaviour:
+# An example of expected behaviour:
 
-Sample output
-Please type in a string: hiya
-a
-y
-i
-h
+# Sample output
+# Please type in a string: hiya
+# a
+# y
+# i
+# h
+
+a = input(" Enter the string")
+index = 0
+while True:
+    print(a[index])
+    index += 1
