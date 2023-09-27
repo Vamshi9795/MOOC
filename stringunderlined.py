27c2e03ea0f9064be4be146aa2a24468ffc106a7
Vamshi9795 <chjyothendar@gmail.com>
2023-09-27 18:53:29 -0500

    made a while loop to iterate

--- stringunderlined.py
+++ stringunderlined.py
@@ -1,16 +1,23 @@
-<!-- Please write a program which asks the user for strings using a loop. The program prints out each string underlined as shown in the examples below. The execution ends when the user inputs an empty string - that is, just presses Enter at the prompt.
+# <!-- Please write a program which asks the user for strings using a loop. The program prints out each string underlined as shown in the examples below. The execution ends when the user inputs an empty string - that is, just presses Enter at the prompt.
 
-Sample output
-Please type in a string: Hi there!
+# Sample output
+# Please type in a string: Hi there!
 
-Hi there!
----------
-Please type in a string: This is a test
+# Hi there!
+# ---------
+# Please type in a string: This is a test
 
-This is a test
---------------
-Please type in a string: a
+# This is a test
+# --------------
+# Please type in a string: a
 
-a
--
-Please type in a string: -->
+# a
+# -
+# Please type in a string: -->
+ 
+while True:
+    a = input(" Please type in the string")
+    print(f" a \n {len(a)}")
+    if a == "quit":
+        print(" Exiting the loop")
+    break
