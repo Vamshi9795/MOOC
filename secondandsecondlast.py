3940ce24f9ae209c59acb08db7d89073a3e63b85
Vamshi9795 <chjyothendar@gmail.com>
2023-09-27 11:40:40 -0500

    added if else statement

---
+++ secondandsecondlast.py
@@ -0,0 +1,14 @@
+# Please write a program which asks the user for a string. The program then prints out a message based on whether the second character and the second to last character are the same or not. See the examples below.
+
+# Sample output
+# Please type in a string: python
+# The second and the second to last characters are different
+# Sample output
+# Please type in a string: pascal
+# The second and the second to last characters are a
+
+a = input(" Enter the string: ")
+if a[1] == a [-2]:
+    print("The second and the second to last characters are same ")
+else:
+    print(" The second and the second to last characters are different")
