24879e19c3394d45b609a9ba0821baffd9e288f3
Vamshi9795 <chjyothendar@gmail.com>
2023-09-27 11:43:37 -0500

    added logic

---
+++ hasklength.py
@@ -0,0 +1,15 @@
+# Please write a program which prints out a line of hash characters, the width of which is chosen by the user.
+
+# Sample output
+# Width: 3
+# ###
+# Sample output
+# Width: 8
+# ########
+
+a = "#"
+try:
+    b = int(input(" ENter the width"))
+    print(a*b)
+except ValueError:
+    print(" Enter a numeric width")
